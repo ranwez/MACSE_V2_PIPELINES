08701e75c8d012e43956d3a076e43c43ef817076
Vincent Ranwez <vincent.ranwez@supagro.fr>
2020-03-20 18:08:16 +0100

    Update S_getRepresentativeSeqs.sh

--- S_getRepresentativeSeqs.sh
+++ S_getRepresentativeSeqs.sh
@@ -66,7 +66,7 @@ macse="macse_v2.03.jar";
 ## identify sequences similar to the reference one
 ##############################################################
 $seqtk
-$mmseqs2
+$mmseqss
 $macse
 java -jar -Xmx800m $macse -prog translateNT2AA -gc_def $in_geneticCode -seq $in_refSeq -out_AA ref_seq_AA.fasta
 
