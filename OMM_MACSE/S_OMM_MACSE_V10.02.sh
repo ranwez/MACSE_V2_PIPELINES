f5dbdfd9a13beb9ecd27b7a1ac3115494a6a69a0
Vincent Ranwez <vincent.ranwez@supagro.fr>
2020-03-25 18:10:52 +0100

    Update S_OMM_MACSE_V10.02.sh

--- OMM_MACSE/S_OMM_MACSE_V10.02.sh
+++ OMM_MACSE/S_OMM_MACSE_V10.02.sh
@@ -7,11 +7,12 @@ set -Euo pipefail
 ######################################
 # SCRIPT PARAMETERS
 SCRIPT_NAME=$(basename "$0")
+printf "\n\n" # separate script message from the rest
 
 function quit_pb_option() {
     printf "\n\nThis script aligns sequences using: 1) MACSE pre-filtering, 2) MACSE alignment to find frameshifts, 3) MAFFT for aligning the resulting AA sequences, and 4) HMMcleaner for cleaning resulting alignments.\n"
     printf "\nyour command line is incorrect please check your options"
-    printf "\n usage example:\n$SCRIPT_NAME --out_dir out_dir --out_file_prefix out_file_prefix --in_seq_file seq_file.fasta [--genetic_code_number code_number] [--alignAA_soft MAFFT/MUSCLE] [--min_percent_NT_at_ends 0.7] [--out_detail_dir SAVE_DETAILS/] [--in_seq_lr_file less_reliable_seq_file.fasta] [--java_mem 500m] [--no_prefiltering] [--no_FS_detection] [--no_filtering] [--no_postfiltering] [--replace_FS_by_gaps] [--save_details]\n"
+    printf "\n usage example:\n$SCRIPT_NAME --out_dir out_dir --PREFIX PREFIX --in_seq_file seq_file.fasta [--genetic_code_number code_number] [--alignAA_soft MAFFT/MUSCLE] ][--aligner_extra_option] [--min_percent_NT_at_ends 0.7] [--out_detail_dir SAVE_DETAILS/] [--in_seq_lr_file less_reliable_seq_file.fasta] [--java_mem 500m] [--no_prefiltering] [--no_FS_detection] [--no_filtering] [--no_postfiltering] [--replace_FS_by_gaps] [--save_details]\n"
     printf "\n\nFor further details please check the documentation on MACSE website: https://bioweb.supagro.inra.fr/macse\n\n"
     exit 1
 }
@@ -26,49 +27,44 @@ source "$LG_UTILS"/S_utilIO.sh
 #handle parameters
 
 PRE_FILTERING=1; POST_FILTERING=1; FILTERING=1; FS_DETECTION=1; REPLACE_FS_GAP=0
-HAS_SEQ_LR=0; SEQ_LR_OPT=""
-ALIGNER_EXTRA_OPTION=""
-GC_OPT=""
-SAVE_DETAILS=0
-JAVA_MEM=250m
-REPLACE_FS_GAP=0
-ALIGN_SOFT="MAFFT"
-MIN_PERCENT_NT_AT_ENDS=0.7
+HAS_SEQ_LR=0; SEQ_LR_OPT="";GC_OPT=""
+ALIGN_SOFT="MAFFT"; ALIGNER_EXTRA_OPTION=""
+SAVE_DETAILS=0; debug=0;JAVA_MEM=250m
+MIN_PERCENT_NT_AT_ENDS=0.7; REPLACE_FS_GAP=0
 
 
 while (( $# > 0 )); do
     case "$1" in
 	     --in_seq_file)                IN_SEQ_FILE=$(get_in_file_param "$1" "$2")               || quit_pb_option ; shift 2;;
-	     --in_seq_lr_file)             IN_SEQ_LR_FILE=$(get_in_file_param "$1" "$2")            || quit_pb_option ; HAS_SEQ_LR=1; SEQ_LR_OPT="-seq_lr $IN_SEQ_LR_FILE"shift 2;;
-	     --in_geneticCode)             in_geneticCode=$(get_in_int_param "$1" "$2")             || quit_pb_option ; GC_OPT=" -gc_def $in_geneticCode"; shift 2;;
+	     --in_seq_lr_file)             IN_SEQ_LR_FILE=$(get_in_file_param "$1" "$2")            || quit_pb_option ; HAS_SEQ_LR=1; SEQ_LR_OPT="-seq_lr $IN_SEQ_LR_FILE"; shift 2;;
+	     --genetic_code_number)        in_geneticCode=$(get_in_int_param "$1" "$2")             || quit_pb_option ; GC_OPT=" -gc_def $in_geneticCode"; shift 2;;
        --java_mem)                   JAVA_MEM="$2"                                            || quit_pb_option ; shift 2;;
-       --out_dir)                    OUT_DIR=$(get_out_file_param "$1" "$2")                  || quit_pb_option ; shift 2;;
-       --out_file_prefix)            OUT_FILE_PREFIX="$2"                                     || quit_pb_option ; shift 2;;
+       --out_dir)                    OUT_DIR=$(get_out_dir_param "$1" "$2")                  || quit_pb_option ; shift 2;;
+       --out_file_prefix)            PREFIX="$2"                                              || quit_pb_option ; shift 2;;
        --min_percent_NT_at_ends)     MIN_PERCENT_NT_AT_ENDS="$2"                              || quit_pb_option ; shift 2;;
        --out_detail_dir)             OUT_DETAIL_DIR=$(get_out_file_param "$1" "$2")           || quit_pb_option ; SAVE_DETAILS=1; shift 2;;
-       --no_prefiltering)            PRE_FILTERING=0;                                                           ; shift 1;;
-       --no_FS_detection)            FS_DETECTION=0;                                                            ; shift 1;;
-       --no_filtering)               FILTERING=0;                                                               ; shift 1;;
-       --no_postfiltering)           POST_FILTERING=0;                                                          ; shift 1;;
-       --replace_FS_by_gaps)         REPLACE_FS_GAP=1;                                                          ; shift 1;;
+       --no_prefiltering)            PRE_FILTERING=0                                                            ; shift 1;;
+       --no_FS_detection)            FS_DETECTION=0                                                             ; shift 1;;
+       --no_filtering)               FILTERING=0                                                                ; shift 1;;
+       --no_postfiltering)           POST_FILTERING=0                                                           ; shift 1;;
+       --replace_FS_by_gaps)         REPLACE_FS_GAP=1                                                           ; shift 1;;
        --debug)                      debug=1                                                                    ; shift 1;;
-       --save_details)               SAVE_DETAILS=1;                                                            ; shift 1;;
+       --save_details)               SAVE_DETAILS=1                                                             ; shift 1;;
        --alignAA_soft)               ALIGN_SOFT="$2"                                           || quit_pb_option; shift 2
            if [[ ! "$ALIGN_SOFT" =~ ^(MAFFT|MUSCLE)$ ]];  then
              echo " Alignment software $ALIGN_SOFT is not handle, please choose between MAFFT or MUSCLE"; quit_pb_option
            fi
            ;;
-       --aligner_extra_option)       ALIGNER_EXTRA_OPTION="$2"                                || quit_pb_option ; shift 1;;
+       --aligner_extra_option)       ALIGNER_EXTRA_OPTION="$2"                                || quit_pb_option ; shift 2;;
 	      *) printf "Option $1 is unknown please ckeck your command line"; quit_pb_option;;
     esac
 done
 
 # check that mandatory parameters are set
 # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ -z ${in_seq_file+x}  ];    then printf "mandatory --in_seq_file is missing";     quit_pb_option; fi
-if [ -z ${out_dir+x} ];         then printf "mandatory --out_dir is missing";         quit_pb_option; fi
-if [ -z ${out_file_prefix+x} ]; then printf "mandatory --out_file_prefix is missing"; quit_pb_option; fi
-
+if [ -z ${IN_SEQ_FILE+x}  ];    then printf "mandatory --in_seq_file is missing";     quit_pb_option; fi
+if [ -z ${OUT_DIR+x} ];         then printf "mandatory --out_dir is missing";         quit_pb_option; fi
+if [ -z ${PREFIX+x} ]; then printf "mandatory --PREFIX is missing"; quit_pb_option; fi
 
 
 printf "============================ PROCESSING $PREFIX\n"
@@ -89,19 +85,30 @@ cd $tmp_dir
 ######################################
 #filter sequences and identify frameshift using MACSE
 
+touch __${PREFIX}_homol_tmp_NT_lr.fasta
+
+cp $IN_SEQ_FILE __all_seq.fasta;
+if (( $HAS_SEQ_LR > 0 )); then
+    echo "" >> __all_seq.fasta; cat "$IN_SEQ_LR_FILE">> __all_seq.fasta;
+fi
+
 if(( $PRE_FILTERING > 0)); then
     printf "\n\n============== MACSE PRE_FILTERING\n"
-    $macse -prog trimNonHomologousFragments $GC_OPT -seq $IN_SEQ_FILE $SEQ_LR_OPT -out_NT __${PREFIX}_homol_tmp_NT.fasta -out_AA __${PREFIX}_homol_tmp_AA.fasta -out_trim_info __${PREFIX}_homol_fiter.csv -out_mask_detail __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta -min_trim_in 60 -min_trim_ext 45 -debug
-
+    if (( $HAS_SEQ_LR > 0 )); then
+        $macse -prog trimNonHomologousFragments $GC_OPT -seq __all_seq.fasta -out_NT __${PREFIX}_homol_tmp_NT_all.fasta -out_AA __${PREFIX}_homol_tmp_all_AA.fasta -out_trim_info __${PREFIX}_homol_fiter.csv -out_mask_detail __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta -min_trim_in 60 -min_trim_ext 45 -debug
+        for s in $(grep ">" $IN_SEQ_FILE); do grep -A1 -e"$s$" __${PREFIX}_homol_tmp_NT_all.fasta; done >  __${PREFIX}_homol_tmp_NT.fasta
+        for s in $(grep ">" $IN_SEQ_LR_FILE); do grep -A1 -e"$s$" __${PREFIX}_homol_tmp_NT_all.fasta; done >  __${PREFIX}_homol_tmp_NT_lr.fasta
+    else
+      $macse -prog trimNonHomologousFragments $GC_OPT -seq __all_seq.fasta -restrict __reliable.list -out_NT __${PREFIX}_homol_tmp_NT.fasta -out_AA __${PREFIX}_homol_tmp_all_AA.fasta -out_trim_info __${PREFIX}_homol_fiter.csv -out_mask_detail __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta -min_trim_in 60 -min_trim_ext 45 -debug
+    fi
 else
     cp $IN_SEQ_FILE __${PREFIX}_homol_tmp_NT.fasta
     if (( $HAS_SEQ_LR > 0 )); then
-        echo "" >> __${PREFIX}_homol_tmp_NT.fasta
-        cat "$IN_SEQ_LR_FILE">> __${PREFIX}_homol_tmp_NT.fasta
+        cp "$IN_SEQ_LR_FILE" __${PREFIX}_homol_tmp_NT_lr.fasta
     fi
 
     echo "seqName;initialSeqLength;nbKeep;nbTrim;nbInformativeTrim;percentHomologExcludingExtremities;percentHomologIncludingExtremities;keptSequences" >__${PREFIX}_homol_fiter.csv
-    for s in $(grep ">" __${PREFIX}_homol_tmp_NT.fasta | cut -f1 -d">"); do
+    for s in $(grep ">" __all_seq.fasta | cut -f1 -d">"); do
           echo "${s};NA;NA;NA;NA;NA;NA;NA;true" >>__${PREFIX}_homol_fiter.csv
           $LG_UTILS/LGS_Fasta/S_unmask_seq.sh --in_seq_file __${PREFIX}_homol_tmp_NT.fasta --out_seq_file __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta
     done
@@ -110,10 +117,12 @@ fi
 
 if(( FS_DETECTION > 0 )); then
   printf "\n\n============== MACSE FRAMESHIFT DETECTION\n"
-  $macse -prog alignSequences $GC_OPT -seq __${PREFIX}_homol_tmp_NT.fasta -optim 2 -max_refine_iter 3 -local_realign_init 0.2 -out_NT __${PREFIX}_homol_tmp_NT.aln -out_AA __${PREFIX}_homol_tmp_AA.aln
+  echo "$macse -prog alignSequences $GC_OPT -seq __${PREFIX}_homol_tmp_NT.fasta -seq_lr __${PREFIX}_homol_tmp_NT_lr.fasta -optim 2 -max_refine_iter 3 -local_realign_init 0.2 -out_NT __${PREFIX}_homol_tmp_NT.aln -out_AA __${PREFIX}_homol_tmp_AA.aln"
+  $macse -prog alignSequences $GC_OPT -seq __${PREFIX}_homol_tmp_NT.fasta -seq_lr __${PREFIX}_homol_tmp_NT_lr.fasta -optim 2 -max_refine_iter 3 -local_realign_init 0.2 -out_NT __${PREFIX}_homol_tmp_NT.aln -out_AA __${PREFIX}_homol_tmp_AA.aln
+
   #check if it is fine or not
   if [ ! -f __${PREFIX}_homol_tmp_NT.aln ] ; then
-      echo "Something goes wrong with MACSE alignSequences subprogram. This is generally link to memory problems. Try to re-launch the script with extra memory using the --java_mem option: e.g. --java_mem 2000m "
+      printf "\n\nSomething goes wrong with MACSE alignSequences subprogram.\nIn absence of other error message, this is generally link to memory problems.\n Try to re-launch the script with extra memory using the --java_mem option: e.g. --java_mem 2000m\n"
       exit 1;
   fi
   #prepare fasta file
@@ -179,33 +188,33 @@ else
 
 fi
 
-cp __${PREFIX}_final_homol_NTOK.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_mask_align_NT.aln
-cp __${PREFIX}_final_homol_AAOK.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_mask_align_AA.aln
+cp __${PREFIX}_final_homol_NTOK.aln $OUT_DIR/${PREFIX}_final_mask_align_NT.aln
+cp __${PREFIX}_final_homol_AAOK.aln $OUT_DIR/${PREFIX}_final_mask_align_AA.aln
 
-cp __${PREFIX}_final_homol_NTOK_export.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_align_NT.aln
-cp __${PREFIX}_final_homol_AAOK_export.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_align_AA.aln
+cp __${PREFIX}_final_homol_NTOK_export.aln $OUT_DIR/${PREFIX}_final_align_NT.aln
+cp __${PREFIX}_final_homol_AAOK_export.aln $OUT_DIR/${PREFIX}_final_align_AA.aln
 
 printf "\n\n============== MACSE merge masks if needed \n"
 
 
 if(( ${PRE_FILTERING} > 0 || ${FILTERING} > 0)); then
-  cp __${PREFIX}_unmask_NT.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_unmask_align_NT.aln
-  cp __${PREFIX}_unmask_AA.aln $OUT_DIR/${OUT_FILE_PREFIX}_final_unmask_align_AA.aln
+  cp __${PREFIX}_unmask_NT.aln $OUT_DIR/${PREFIX}_final_unmask_align_NT.aln
+  cp __${PREFIX}_unmask_AA.aln $OUT_DIR/${PREFIX}_final_unmask_align_AA.aln
 fi
 
 if(( ${PRE_FILTERING} > 0 && ${FILTERING} == 0 )); then
     ${LG_UTILS}/LGS_Fasta/S_mask_removed_seq.sh --in_seq_file __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta --in_keep_seq_info __${PREFIX}_homol_fiter.csv --out_seq_file __${PREFIX}_NonHomolFilter_RmSeq_NT_mask_detail.fasta --col_keep_info 8
-    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${OUT_FILE_PREFIX}_maskHomolog_stat.csv
-    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${OUT_FILE_PREFIX}_maskFull_stat.csv
-    cp __${PREFIX}_NonHomolFilter_RmSeq_NT_mask_detail.fasta $OUT_DIR/${OUT_FILE_PREFIX}_maskFull_detail.fasta
+    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${PREFIX}_maskHomolog_stat.csv
+    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${PREFIX}_maskFull_stat.csv
+    cp __${PREFIX}_NonHomolFilter_RmSeq_NT_mask_detail.fasta $OUT_DIR/${PREFIX}_maskFull_detail.fasta
 fi
 
 if(( ${FILTERING} > 0 )); then
     $LG_UTILS/LGS_Fasta/S_mask_removed_seq.sh --in_seq_file __${PREFIX}_NonHomolFilter_NT_mask_detail.fasta --in_keep_seq_info __${PREFIX}_homol_fiter.csv --out_seq_file __${PREFIX}_NonHomolFilter_RmSeq_NT_mask_detail.fasta --col_keep_info 8
     $macse -prog mergeTwoMasks -mask_file1 __${PREFIX}_NonHomolFilter_RmSeq_NT_mask_detail.fasta -mask_file2 __${PREFIX}_hmmCleaner_mask2_detail.aln -out_mask_detail __${PREFIX}_maskFull_detail.fasta -out_trim_info __${PREFIX}_maskFull_stat.csv
-    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${OUT_FILE_PREFIX}_maskHomolog_stat.csv
-    cp __${PREFIX}_maskFull_stat.csv $OUT_DIR/${OUT_FILE_PREFIX}_maskFull_stat.csv
-    cp __${PREFIX}_maskFull_detail.fasta $OUT_DIR/${OUT_FILE_PREFIX}_maskFull_detail.fasta
+    cp __${PREFIX}_homol_fiter.csv $OUT_DIR/${PREFIX}_maskHomolog_stat.csv
+    cp __${PREFIX}_maskFull_stat.csv $OUT_DIR/${PREFIX}_maskFull_stat.csv
+    cp __${PREFIX}_maskFull_detail.fasta $OUT_DIR/${PREFIX}_maskFull_detail.fasta
 fi
 
 cp $script_dir/readme_output.txt $OUT_DIR/
